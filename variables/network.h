c8c4b345e998107f3c212b2907e84cc16b7fc68e
Eric Thieme-Garmann <erictg96@googlemail.com>
2021-11-08 15:46:06 +0100

    Update network.h

--- variables/network.h
+++ variables/network.h
@@ -1,7 +1,10 @@
 
 /**
  * Ändere die WLAN Infos hier 
- */
+* Example:
+* const char *ssid = "MyNetwork";
+* const char *pass = "superSafePassword2021";
+*/
 
 const char *ssid = "";
 const char *pass = "";
