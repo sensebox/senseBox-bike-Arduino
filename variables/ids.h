7322b8832bf9d0833ff8602e8c9a8cc87f2907c4
Eric Thieme-Garmann <erictg96@googlemail.com>
2021-11-08 15:45:18 +0100

    Added examples for variable assignment

--- variables/ids.h
+++ variables/ids.h
@@ -1,11 +1,14 @@
 /** Ändere die ID's der Sensoren hier
- * 
+ * Sensor IDs können über die API abgefragt werden 
+ * https://api.opensensemap.org/boxes/[:senseBoxID]
  */
 
 // senseBox ID
+// example : const char SENSEBOX_ID[] = "615f292bc031ff001b117192";
 const char SENSEBOX_ID[] = "";
 
 // Sensor ID's
+// example : char tempID[] = "615f292bc031ff001b11719b";
 char tempID[] = "";
 char humiID[] = "";
 char distLeftID[] = "";
