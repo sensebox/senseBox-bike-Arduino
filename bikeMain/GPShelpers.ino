5f4f8f896cfe21e2437bf4bd8b01a499b101866c
Thiemann96 <e_thie10@uni-muenster.de>
2023-03-17 15:33:32 +0100

    added geofencing support to sketch

--- bikeMain/GPShelpers.ino
+++ bikeMain/GPShelpers.ino
@@ -31,3 +31,31 @@ void setGPS(){
   strcpy_P(latGlobal, lat);
   
 }
+
+void setGPS(){
+  double latitude = fix.latitude();
+  double longitude = fix.longitude();
+  char lng[20];
+  char lat[20];
+  double homeLon[] = {minLon, minLon, maxLon, maxLon};
+  double homeLat[] = {minLat, maxLat, maxLat, minLat};
+
+  // set GPS to fixed position, when the actual position is inside the fence.
+  if (defLat > 1 && defLon > 1) { // check if default coordiante is set.
+    if (pnpoly(4, homeLat, homeLon, latitude, longitude)) {
+      dtostrf(defLon, 2, 7, lng);
+      dtostrf(defLat, 1, 7, lat);
+    } else {
+      dtostrf(longitude, 2, 7, lng);
+      dtostrf(latitude, 1, 7, lat);
+    }
+  } else {
+      dtostrf(longitude, 2, 7, lng);
+      dtostrf(latitude, 1, 7, lat);
+  }
+
+
+  strcpy_P(lngGlobal, lng);
+  strcpy_P(latGlobal, lat);
+  
+}
