bec8f75dd196e01703e7c31122755f4b26294d48
Felix Erdmann <hello@felixerdmann.com>
2024-06-11 14:10:20 +0200

    fix: distance sensor merge

--- senseBox-bike-atrai-v2/src/sensors/DistanceSensor/DistanceSensor.cpp
+++ senseBox-bike-atrai-v2/src/sensors/DistanceSensor/DistanceSensor.cpp
@@ -8,7 +8,6 @@ int distanceCharacteristic = 0;
 String overtakingUUID = "FC01C6882C444965AE18373AF9FED18D";
 int overtakingCharacteristic = 0;
 
-
 #include "model_data.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -25,14 +24,14 @@ int overtakingCharacteristic = 0;
 VL53L8CX sensor_vl53l8cx_top(&Wire, -1, -1);
 const int kChannelNumber = 64;
 const int kFrameNumber = 20;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
-TfLiteTensor* model_input = nullptr;
+const tflite::Model *model = nullptr;
+tflite::MicroInterpreter *interpreter = nullptr;
+TfLiteTensor *model_input = nullptr;
 int input_length;
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
 // determined by experimentation.
-constexpr int kTensorArenaSize = 14 * 1024 + 1008 ;
+constexpr int kTensorArenaSize = 14 * 1024 + 1008;
 uint8_t tensor_arena[kTensorArenaSize];
 
 // A buffer holding the last 20 sets of 8x8 pixels
@@ -60,10 +59,11 @@ void DistanceSensor::initSensor()
   // -------------------------- setup tensorflow model --------------------------
   Serial.println("setting up tensorflow...");
   model = tflite::GetModel(g_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
     Serial.printf("Model provided is schema version %d not equal "
-                         "to supported version %d.",
-                         model->version(), TFLITE_SCHEMA_VERSION);
+                  "to supported version %d.",
+                  model->version(), TFLITE_SCHEMA_VERSION);
     return;
   }
   static tflite::MicroErrorReporter tflErrorReporter;
@@ -72,7 +72,7 @@ void DistanceSensor::initSensor()
   static tflite::AllOpsResolver resolver;
   // Build an interpreter to run the model with.
   static tflite::MicroInterpreter static_interpreter(
-      model, resolver, (uint8_t*)(tensor_arena), (size_t)(kTensorArenaSize), &tflErrorReporter);
+      model, resolver, (uint8_t *)(tensor_arena), (size_t)(kTensorArenaSize), &tflErrorReporter);
   interpreter = &static_interpreter;
   // Allocate memory from the tensor_arena for the model's tensors.
   interpreter->AllocateTensors();
@@ -80,8 +80,9 @@ void DistanceSensor::initSensor()
   model_input = interpreter->input(0);
   if ((model_input->dims->size != 3) || (model_input->dims->data[0] != 1) ||
       (model_input->dims->data[1] != kFrameNumber) ||
-      (model_input->dims->data[2] != kChannelNumber) || 
-      (model_input->type != kTfLiteFloat32)) {
+      (model_input->dims->data[2] != kChannelNumber) ||
+      (model_input->type != kTfLiteFloat32))
+  {
     Serial.println(model_input->dims->size);
     Serial.println(model_input->dims->data[0]);
     Serial.println(model_input->dims->data[1]);
@@ -109,7 +110,7 @@ void DistanceSensor::readSensorData()
   float distance = -1.0;
 
   if ((!status) && (NewDataReady != 0))
-  {    
+  {
     sensor_vl53l8cx_top.vl53l8cx_get_ranging_data(&Results);
     float overtakingPredictionPercentage = -1.0;
     float oldVl53l8cxMin = -1.0;
@@ -120,20 +121,24 @@ void DistanceSensor::readSensorData()
       {
         for (int k = (8 - 1); k >= 0; k--)
         {
-          if ((float)(&Results)->target_status[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l] != 255)
+          if ((float)(&Results)->target_status[(VL53L8CX_NB_TARGET_PER_ZONE * (j + k)) + l] != 255)
           {
-            if((float)(&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l] > 2000.0){
+            if ((float)(&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j + k)) + l] > 2000.0)
+            {
               save_data[begin_index++] = 0.0;
-            } else {
-              save_data[begin_index++] = (float)(&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l];
             }
-            float distance = ((&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l])/10;
+            else
+            {
+              save_data[begin_index++] = (float)(&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j + k)) + l];
+            }
+            float distance = ((&Results)->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j + k)) + l]) / 10;
             if (min > distance)
             {
               min = distance;
             }
           }
-          else {
+          else
+          {
             save_data[begin_index++] = 0.0;
           }
         }
@@ -142,51 +147,55 @@ void DistanceSensor::readSensorData()
     oldVl53l8cxMin = (min == 10000.0) ? 0.0 : min;
 
     // If we reached the end of the circle buffer, reset
-    if (begin_index >= (RING_BUFFER_SIZE)) {
+    if (begin_index >= (RING_BUFFER_SIZE))
+    {
       begin_index = 0;
       // Check if we are ready for prediction or still pending more initial data
-      if (pending_initial_data) {
+      if (pending_initial_data)
+      {
         pending_initial_data = false;
       }
     }
-    if (!pending_initial_data) {
-      for (int i = 0; i < input_length; ++i) {
+    if (!pending_initial_data)
+    {
+      for (int i = 0; i < input_length; ++i)
+      {
         int ring_array_index = begin_index + i - input_length;
-        if (ring_array_index < 0) {
+        if (ring_array_index < 0)
+        {
           ring_array_index += (RING_BUFFER_SIZE);
         }
         // normalize
-        model_input->data.f[i] = save_data[ring_array_index]/2000.0;
+        model_input->data.f[i] = save_data[ring_array_index] / 2000.0;
       }
       // Run inference, and report any error.
       TfLiteStatus invoke_status = interpreter->Invoke();
-      if (invoke_status == kTfLiteOk) {
-        const float* prediction_scores = interpreter->output(0)->data.f;
+      if (invoke_status == kTfLiteOk)
+      {
+        const float *prediction_scores = interpreter->output(0)->data.f;
         overtakingPredictionPercentage = prediction_scores[0];
       }
     }
-    distance = (min == 10000.0) ? 0.0 : min / 10.0;
-    Serial.printf("Distance: %.2f cm\n", distance);
-  }
 
-    float distance = oldVl53l8cxMin;
+    oldVl53l8cxMin = (min == 10000.0) ? 0.0 : min;
 
     if (measurementCallback)
     {
       measurementCallback({distance, overtakingPredictionPercentage});
     }
 
-    if (sendBLE && (millis()-prevBleTime)>=1000)
+    if (sendBLE && (millis() - prevBleTime) >= 1000)
     {
-      notifyBLE(distance,overtakingPredictionPercentage);
+      notifyBLE(distance, overtakingPredictionPercentage);
       prevBleTime = millis();
     }
   }
-  if((millis()-prevMeasureTime)<65){
-    vTaskDelay(pdMS_TO_TICKS(65-(millis()-prevMeasureTime)));
+  if ((millis() - prevMeasureTime) < 65)
+  {
+    vTaskDelay(pdMS_TO_TICKS(65 - (millis() - prevMeasureTime)));
   }
-  Serial.println(millis()-prevMeasureTime);
-  prevMeasureTime=millis();
+  Serial.println(millis() - prevMeasureTime);
+  prevMeasureTime = millis();
 }
 
 void DistanceSensor::notifyBLE(float distance, float overtakingPredictionPercentage)
