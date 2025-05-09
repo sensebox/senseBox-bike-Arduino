ad0a7fc8cd2490e648aee5eb193efe04a98336c7
Felix Erdmann <hello@felixerdmann.com>
2025-05-09 15:55:59 +0200

    feat: auto detect acceleration sensor

--- senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.cpp
+++ senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.cpp
@@ -4,29 +4,32 @@
 AccelerationSensor::AccelerationSensor() : BaseSensor("accelerationSensorTask", 2048, 0) {}
 
 String surfaceClassificationUUID = "B944AF10F4954560968F2F0D18CAB521";
-// String accUUID = "B944AF10F4954560968F2F0D18CAB522";
 String anomalyUUID = "B944AF10F4954560968F2F0D18CAB523";
 int surfaceClassificationCharacteristic = 0;
 int anomalyCharacteristic = 0;
 
-Adafruit_MPU6050 mpu;
-
 void AccelerationSensor::initSensor()
 {
-
-  if (!mpu.begin(0x68, &Wire1))
-  {
-    Serial.println("MPU6050 Chip wurde nicht gefunden");
-    return;
-  }
-  else
+  // Try initializing MPU6050 first
+  if (mpu.begin(0x68, &Wire1))
   {
     Serial.println("MPU6050 Found!");
     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
     mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    delay(100);
-  };
+    activeSensor = MPU6050;
+  }
+  else if (icm.begin_I2C(0x68, &Wire1))
+  {
+    // If MPU6050 fails, try ICM20948
+    Serial.println("ICM20948 Found!");
+    activeSensor = ICM20948;
+  }
+  else
+  {
+    Serial.println("No compatible sensor found");
+    return;
+  }
 
   surfaceClassificationCharacteristic = BLEModule::createCharacteristic(surfaceClassificationUUID.c_str());
   anomalyCharacteristic = BLEModule::createCharacteristic(anomalyUUID.c_str());
@@ -48,23 +51,40 @@ bool AccelerationSensor::readSensorData()
   bool classified = false;
   sensors_event_t a, g, temp;
 
-  mpu.getEvent(&a, &g, &temp);
-
-  buffer[ix++] = a.acceleration.x;
-  buffer[ix++] = a.acceleration.y;
-  buffer[ix++] = a.acceleration.z;
-  buffer[ix++] = g.gyro.x;
-  buffer[ix++] = g.gyro.y;
-  buffer[ix++] = g.gyro.z;
+  if (activeSensor == MPU6050)
+  {
+    mpu.getEvent(&a, &g, &temp);
+    buffer[ix++] = a.acceleration.x;
+    buffer[ix++] = a.acceleration.y;
+    buffer[ix++] = a.acceleration.z;
+    buffer[ix++] = g.gyro.x;
+    buffer[ix++] = g.gyro.y;
+    buffer[ix++] = g.gyro.z;
+  }
+  else if (activeSensor == ICM20948)
+  {
+    icm.getEvent(&a, &g, &temp);
+    buffer[ix++] = a.acceleration.x;
+    buffer[ix++] = a.acceleration.y;
+    buffer[ix++] = a.acceleration.z;
+    buffer[ix++] = g.gyro.x;
+    buffer[ix++] = g.gyro.y;
+    buffer[ix++] = g.gyro.z;
+  }
+  else
+  {
+    return false; // No sensor active
+  }
 
   Serial.println(millis() - prevAccTime);
   prevAccTime = millis();
 
-  // one second inverval
+  // one second interval
   if (EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE <= ix)
   {
     classified = true;
-    // Turn the raw buffer in a signal which we can the classify
+
+    // Turn the raw buffer in a signal which we can classify
     signal_t signal;
     int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
     if (err != 0)
@@ -76,7 +96,6 @@ bool AccelerationSensor::readSensorData()
 
     // Run the classifier
     ei_impulse_result_t result = {};
-
     err = run_classifier(&signal, &result, false);
     if (err != EI_IMPULSE_OK)
     {
@@ -95,12 +114,10 @@ bool AccelerationSensor::readSensorData()
 
     if (sendBLE)
     {
-
       notifyBLE(probAsphalt, probCompact, probPaving, probSett, probStanding, anomaly);
     }
 
     ix = 0;
-
     buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = {};
   }
 
--- senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.h
+++ senseBox-bike-atrai-v2/src/sensors/AccelerationSensor/AccelerationSensor.h
@@ -3,6 +3,7 @@
 
 #include "../BaseSensor.h"
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_ICM20948.h> // Include for ICM20948
 
 class AccelerationSensor : public BaseSensor
 {
@@ -13,6 +14,18 @@ public:
 protected:
   void initSensor() override;
   void notifyBLE(float probAsphalt, float probCompact, float probPaving, float probSett, float probStanding, float anomaly);
+
+private:
+  enum SensorType
+  {
+    NONE,
+    MPU6050,
+    ICM20948
+  };
+
+  SensorType activeSensor = NONE; // Track which sensor is active
+  Adafruit_MPU6050 mpu;
+  Adafruit_ICM20948 icm;
 };
 
 #endif // ACCELERATION_SENSOR_H
