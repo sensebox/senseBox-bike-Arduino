2bd7087793264d0ed9fb44a9a49e42af4a9fa5d3
Paula Scharf <paulascharf@gmx.de>
2026-01-19 11:11:37 +0100

    feat: icm20948

--- senseBox-bike-nancy/src/sensors/AccelerationSensor/AccelerationSensor.h
+++ senseBox-bike-nancy/src/sensors/AccelerationSensor/AccelerationSensor.h
@@ -4,6 +4,8 @@
 #include "../BaseSensor.h"
 #include <Adafruit_MPU6050.h>
 #include "ICM42670P.h" // Include for ICM42670P
+#include <Adafruit_ICM20948.h> // Include for ICM20948
+#include <Adafruit_Sensor.h> // Include for ICM20948
 
 class AccelerationSensor : public BaseSensor
 {
@@ -20,12 +22,14 @@ private:
   {
     NONE,
     MPU6050,
-    ICM42670X // ICM42670X is a placeholder for the ICM42670P
+    ICM42670X, // ICM42670X is a placeholder for the ICM42670P
+    ICM20948
   };
 
   SensorType activeSensor = NONE; // Track which sensor is active
   Adafruit_MPU6050 mpu;
   ICM42670 icm = ICM42670(Wire1, 0); // Instantiate an ICM42670 with LSB address set to 0
+  Adafruit_ICM20948 icm2;
 };
 
 #endif // ACCELERATION_SENSOR_H
