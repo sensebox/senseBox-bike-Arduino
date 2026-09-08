38f1d6842673f678ca1f5db5b631d15327d5612f
Paula Scharf <paulascharf@gmx.de>
2026-09-08 13:07:53 +0200

    refactor: clean up

--- senseBox-bike-atrai-v2/src/sensors/DistanceSensor/DistanceSensor.h
+++ senseBox-bike-atrai-v2/src/sensors/DistanceSensor/DistanceSensor.h
@@ -4,6 +4,16 @@
 #include "../BaseSensor.h"
 #include <Wire.h>
 #include <vl53l8cx.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+// #include <TensorFlowLite_ESP32.h>
+#include "edge-impulse-sdk/tensorflow/lite/micro/kernels/micro_ops.h"
+#include "edge-impulse-sdk/tensorflow/lite/micro/micro_interpreter.h"
+#include "edge-impulse-sdk/tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "edge-impulse-sdk/tensorflow/lite/micro/all_ops_resolver.h"
+#include "edge-impulse-sdk/tensorflow/lite/schema/schema_generated.h"
+#include <edge-impulse-sdk/tensorflow/lite/micro/micro_error_reporter.h>
 
 class DistanceSensor : public BaseSensor
 {
